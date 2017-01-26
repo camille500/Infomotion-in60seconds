12aa3f352843e59783ac157b84077603b7ee8556
Camille Sébastien Niessen <camille@overstappen.nl>
2017-01-26 09:11:38 +0100

    Last commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,8 @@
 var voorkeursPartij;
 
 function setExample() {
-  var exampleText = ['Per onderwerp krijgt u drie stellingen te zien. Per stelling kunt u vervolgens aangeven hoe belangrijk u die stelling vindt.',
-                     'U kunt de volgorde van de stellingen achteraf nog aanpassen door ze te verschuiven in de lijst die verschijnt.'];
+  var exampleText = ['Per onderwerp krijg je vier stellingen te zien. Per stelling kun u vervolgens aangeven hoe belangrijk je die stelling vindt.',
+                     'Je kunt de volgorde van de stellingen achteraf nog aanpassen door ze te verschuiven in de lijst die verschijnt.'];
   var exampleImage = ['dist/images/scherm_example_2.svg', 'dist/images/scherm_example_3.svg'];
   var placeholderText = document.getElementById('uitleg_text');
   var placeholderImage = document.getElementById('uitleg_image');
